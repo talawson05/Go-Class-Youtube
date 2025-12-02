b436758df8e3910abc2c4c6579dd19149fcdc159
Tony Lawson <talawson05@gmail.com>
2025-12-02 14:58:17 +0000

    regex

--- main.go
+++ main.go
@@ -2,65 +2,69 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 )
 
-// func fib() func() int {
-// 	a, b := 0, 1
-// 	return func() int {
-// 		a, b = b, a+b
-// 		return b
-// 	}
-// }
-
-// func do(d func()) {
-// 	d()
-// }
-
-func main() {
-	// f := fib()
-	// for x:= f(); x < 100; x = f() {
-	// 	fmt.Println(x)
-	// }
-
-	// f, g := fib(), fib()
-	// fmt.Println(f(), f(),f(),f())
-	// fmt.Println(g(), g(),g(),g())
-
-	// // Prints values of i as 0 through 3, but all live in same address
-	// for i := 0; i < 4; i++ {
-	// 	v := func() {
-	// 		fmt.Printf("%d @ %p\n", i, &i)
-	// 	}
-	// 	do(v)
-	// }
 
 
-	// // GOTCHA: Splitting the creation and calling of a closure, means the value is overwritten
-	// // As i is a reference to i not it's value
-	// // Update: no longer accurate, Go language changed in 1.22 so that variables in for loops will instantiate on every iteration
-	// s := make([]func(), 4)
-	// for i := 0; i < 4; i++ {
-	// 	s[i] = func() {
-	// 		fmt.Printf("%d @ %p \n", i, &i)
-	// 	}
-	// }
+func main() {	
 
-	// for i := 0; i < 4; i++ {
-	// 	s[i]()
-	// }
+	te := "aba abba abbba"
+	re := regexp.MustCompile("b+")
+	mm := re.FindAllString(te, -1)
+	id := re.FindAllStringIndex(te, -1)
 
-	// This way creates a new variable to close over, gives unique addresses
-	s := make([]func(), 4)
-	for i := 0; i < 4; i++ {
-		i2 := i //closure capture
-		s[i] = func() {
-			fmt.Printf("%d @ %p \n", i2, &i2)
+	fmt.Println(mm) // [b bb bbb]
+	fmt.Println(id) // [[1 2] [5 7] [10 13]]
+
+	for _, d := range id {
+		fmt.Println(te[d[0]:d[1]]) // b bb bbb on new lines
+	}
+
+	up := re.ReplaceAllStringFunc(te, strings.ToUpper)
+	fmt.Println(up) // aBa aBBa aBBBa
+
+	// incomplete example
+	ustr := "072665ee-a034-4cc3-a2e8-9f1822c4ebbb"
+	uure := `[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}`
+	ufmt := regexp.MustCompile(uure)
+	if !ufmt.MatchString(ustr) {
+		fmt.Printf("%s is not a UUID\n", ustr)
+	}
+	// could be uppercase
+	// Format is really xxxxxxxx-xxxx-Vxxx-Wxxx-xxxxxxxxxxxx
+	// where V is a version 1-5
+	// Where W is format marker/type: 8,9,a,b
+
+	uu := regexp.MustCompile(`^[[:xdigit:]]{8}-[[:xdigit:]]{4}-[1-5][[:xdigit:]]{3}-[89abAB][[:xdigit:]]{3}-[[:xdigit:]]{12}$`)
+	var test = []string {
+		"072665ee-a034-4cc3-a2e8-9f1822c4ebbb",
+		"072665ee-a034-6cc3-a2e8-9f1822c4ebbb", // incorrect version
+		"072665ee-a034-4cc3-72e8-9f1822c4ebbb", // incorrect type
+		"072665ee-a034-4cc3-a2e8-9f1822c4ebb",  // too short
+		"072665ee-a034-4cc3-a2e8-9f1822c4ebbbb", // too long - GOTCHA valid subsitring, ensure whole string is matched
+		"072665ee-a034-3cc3-82e8-9f1822c4ebbb",
+	}
+
+	for i, t := range test {
+		if !uu.MatchString(t){
+				fmt.Println(i, t, "\tfails")
 		}
 	}
 
-	for i := 0; i < 4; i++ {
-		s[i]()
+	// Capture groups
+	var ph = regexp.MustCompile(`\(([[:digit:]]{3})\) ([[:digit:]]{3})-([[:digit:]]{4})`)
+	orig := "call me at (214) 514-9548 today"
+	match := ph.FindStringSubmatch(orig)
+	fmt.Printf("%q\n", match)
+
+	if len(match) > 3 {
+		// match[0] is the matched string, not the groups
+		fmt.Printf("+1 %s-%s-%s\n", match[1], match[2], match[3])
 	}
 
+	intl := ph.ReplaceAllString(orig, "+1 ${1}-${2}-${3}")
+	fmt.Println(intl)
 
 }
